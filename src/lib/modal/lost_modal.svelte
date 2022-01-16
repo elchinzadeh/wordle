b02cec031e3ff3c5efe745f1d46b70e4c75f9b32
Nijat Rzayev <rzaev.nicat@gmail.com>
2022-01-16 22:01:35 +0400

    svg has been changed iconify library

--- src/lib/modal/lost_modal.svelte
+++ src/lib/modal/lost_modal.svelte
@@ -1,5 +1,6 @@
 <script>
 	import MiniBoard from '$lib/mini-board/mini_board.svelte'
+	import Icon from '@iconify/svelte'
 
 	import { fade } from 'svelte/transition'
 
@@ -21,9 +22,7 @@
 			<div class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
 				<div>
 					<div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100">
-						<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-						</svg>
+						<Icon icon="bi:x" class="h-6 w-6" />
 					</div>
 					<div class="mt-3 text-center sm:mt-5">
 						<h3 class="text-lg leading-6 font-medium text-gray-900">You Lost!</h3>
