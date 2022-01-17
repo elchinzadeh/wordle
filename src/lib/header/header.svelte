fab0385e02f00434ce1b42bb3bca4a7b1d5d0ab9
Nijat Rzayev <rzaev.nicat@gmail.com>
2022-01-17 15:22:31 +0400

    supabase added to test

--- src/lib/header/header.svelte
+++ src/lib/header/header.svelte
@@ -2,10 +2,26 @@
 	import InfoModal from '$lib/modal/info_modal.svelte'
 	import { infoModal } from '../../utils/store'
 	import Icon from '@iconify/svelte'
+	import { SUPABASE_URL, SUPABASE_KEY } from '../../utils/variables';
 
 	function showInfoModal() {
 		infoModal.set(true)
 	}
+
+	import { createClient } from '@supabase/supabase-js'
+
+	async function test(){
+		const supabaseUrl = 'https://oxgmbrsyzojiwwtriddy.supabase.co'
+		const supabaseKey = SUPABASE_KEY
+		const supabase = createClient(supabaseUrl, supabaseKey.toString())
+		const { data, error } = await supabase
+		.from('rooms')
+		.insert([
+			{ name: 'The Shire', code: 5541 }
+		])
+	}
+	
+	
 </script>
 
 <InfoModal />
@@ -16,11 +32,11 @@
 				<Icon icon="ant-design:question-circle-outlined" class="h-8 w-8"/>
 			</div>
 
-			<div class="flex items-center">
+			<div class="flex items-center" >
 				<h1 class="text-4xl font-bold">WORDLE</h1>
 			</div>
 
-			<div class="flex items-center">
+			<div class="flex items-center" on:click="{test}">
 				<Icon icon="ant-design:setting-outlined" class="h-8 w-8"/>	
 			</div>
 		</div>
