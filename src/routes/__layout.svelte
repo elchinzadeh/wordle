7876a444b80d08a2e6e3b758810113a899ca7da3
Nijat Rzayev <rzaev.nicat@gmail.com>
2022-01-23 11:55:27 +0400

    title type is fixed

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <svelte:head>
-	<title>Sözutap - Günlük Söz Oyunu</title>
-	<link href="images/wordle_logo_32x32.png" rel="icon shortcut" sizes="3232" />
-	<link href="images/wordle_logo_192x192.png" rel="apple-touch-icon" />
+	<title>Sözütap - Günlük Söz Oyunu</title>
+	<link href="wordle_logo_32x32.png" rel="icon shortcut" sizes="3232" />
+	<link href="wordle_logo_192x192.png" rel="apple-touch-icon" />
 </svelte:head>
 
 <slot />
