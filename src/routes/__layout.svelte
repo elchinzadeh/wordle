01afac54e857d91bdc32356e1339a42a8f342e48
Nijat Rzayev <rzaev.nicat@gmail.com>
2022-01-23 11:36:36 +0400

    fixed images

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,8 +4,8 @@
 
 <svelte:head>
 	<title>Sözutap - Günlük Söz Oyunu</title>
-	<link href="wordle_logo_32x32.png" rel="icon shortcut" sizes="3232" />
-	<link href="wordle_logo_192x192.png" rel="apple-touch-icon" />
+	<link href="images/wordle_logo_32x32.png" rel="icon shortcut" sizes="3232" />
+	<link href="images/wordle_logo_192x192.png" rel="apple-touch-icon" />
 </svelte:head>
 
 <slot />
